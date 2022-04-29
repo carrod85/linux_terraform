d94d257f45042241203ba031afa10de8bb761d31
Carlos Rodriguez <carrod@ttu.ee>
2022-04-29 19:10:26 +0300

    terraform brief description

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # linux_terraform
+
+
+# Terraform/Ansible tips
+The main use of Terraform is for writing infrastructure as code to manage public cloud resources such as AWS, GCP and Azure.
+
+The infraestrusture can be entirely handled by Terraform or Ansible. Terraform uses cloud provider APIs to create infrastructure and basic configuration tasks are achieved using SSH. The same goes with Ansible – it uses SSH to perform all the required configuration tasks.
+
+Terraform --> declarative code, meaning that it doesnt matter the order the code is written (most of times).
+Ansible --> procedural code, meaning that the script is executed from top to bottom.
+
+# Presentation terraform.
+
+Installation of terraform. You can install it in linux, windows, mac.
+Since I am working on mac. I install with mac using:
+(The brew tap command adds more repositories to the list of formulae that Homebrew tracks, updates, and installs from.)
+    . brew tap hashicorp/tap
+
+    . brew install hashicorp/tap/terraform
+
+Once installed check terraform version by 
+    . terraform -v
+
+
+
+# Create a cloud account. I used AWS
+There is the possibility of using free account, even though you have to insert credit card details.
+
+
+
+    . Implement connexion with api provider
+        https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+      - For a safe connection with cloud. implement installation and configuration of AWS CLI. 
+      - I create a python environment with conda  and install awscli
+      EnvName: terraformEnv
+      conda install -c conda-forge awscli
+      - Checking aws --version
+      - aws configure
+        AWS Access Key ID [None]: 
+        AWS Secret Access Key [None]: 
+        Default region name [None]: us-east-1
+        Default output format [None]: json
+        (terraformEnv)Carloss-MacBook-Pro:~ crflorez$ aws iam get-user (to check user who create it).
+        default root user is used.(didn't create an user in aws)
+
+    . Implement connexion with api provider
+      - Hit terraform init to see that connection is succesful and to download all the necessary plugins to interact with the API
+      - You can track your changes with <terraform plan> command before actually running it.
+
+    . Create a virtual server instance. Selecting the os and the type of instance(power assigned to that vm) I select t2.micro type since it is free:) Ubuntu distribution used.
+
+    . Terraform as well as ansible track changes on the code and only apply differences. A difference though, between them is that with ansible as we said before the code is applied from top to bottom but in terraform it is 'intelligent enough' in many cases to relate tasks that are depending on others, so run them in correct order.
+     . To delete instances you can do either terraform destroy to destroy the whole infrastructure or just delete or uncomment the part of code we don't want anymore.
+Example grabbed from https://github.com/Sanjeev-Thiyagarajan/Terraform-Crash-Course/blob/master/main.tf
+
+     . 1. Creation of VPC. virtual private cloud.
+       2. Create Internet Gateway
+       3. Create Custom Route Table
+       4. Create a Subnet
+       5. Associate subnet with Route Table
+       6. Create Security Group to allow port 22,80,443
+       7. Create a network interface with an ip in the subnet that was created in step 4
+       8. Assign an elastic IP to the network interface created in step 7
+
+
+
+
+
---
+++ main.tf
@@ -0,0 +1,157 @@
+# Configure the AWS Provider
+provider "aws" {
+}
+# all neccesary config like keys and region are saved in home directory, to no make them public when
+# uploading the code online.
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-04505e74c0741db8d"
+#   instance_type = "t2.micro"
+#   tags = {
+#       Name = "Ubuntu"
+#   }
+# } 
+
+# 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+# we apply gateway to the vpc, so we link with the name of the vpc and the id of the object.
+
+
+# 3. Create Custom Route Table, we use default route so all traffic will be redirected if no coincidences
+# to the outside world and won't be dropped.
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+
+# 4. Create a Subnet 
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+
+# 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0 # all ports are allowed.
+    protocol    = "-1" # all protocols are allowed.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+# 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+# output "server_public_ip" {
+#   value = aws_eip.one.public_ip
+# }
+
+# 9. Create Ubuntu server and install/enable apache2
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-04505e74c0741db8d"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "web-server"
+  }
+}
